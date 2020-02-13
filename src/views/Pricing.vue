a3838987e999da679464e96f8418c6b801a2b2b2
shishir75 <shishir.srdr16@gmail.com>
2020-02-13 17:05:04 +0600

    Dynamic Pricing Component Applied

--- src/views/Pricing.vue
+++ src/views/Pricing.vue
@@ -2,39 +2,33 @@
     <div class="flex justify-center">
         <div class="w-1/2 bg-blue-900 rounded-lg shadow px-6 py-12 flex flex-col items-center">
             <div class="bg-gray-400 rounded-full flex justify-around p-1">
-                <button class="rounded-full text-xs font-bold px-6 py-1 uppercase focus:outline-none bg-blue-600 text-gray-200">Starter</button>
-                <button class="rounded-full text-xs font-bold px-6 py-1 uppercase focus:outline-none">Pro</button>
+                <button @click="currentFrequency = frequency "
+                        class="rounded-full text-xs font-bold px-6 py-1 uppercase focus:outline-none"
+                        :class="currentFrequency == frequency ? 'bg-blue-600 text-gray-200' : '' "
+                        v-for="(price, frequency) in plans[0].pricing" v-bind:key="frequency"
+
+                >{{ frequency }}</button>
+
+
             </div>
 
             <div class="flex w-full pt-8">
-                <div class="text-white w-1/2">
-                    <h1 class="text-2xl font-bold">Starter</h1>
+                <div v-for="plan in plans" v-bind:key="plan" class="text-white w-1/2">
+                    <h1 class="text-2xl font-bold">{{ plan.name }}</h1>
                     <ul class="pt-4">
-                        <li>Benefit 1</li>
-                        <li>Benefit 2</li>
-                        <li>Benefit 3</li>
-                        <li>Benefit 4</li>
+                        <li v-for="benefit in plan.benefits[currentFrequency]" v-bind:key="benefit">{{ benefit }}</li>
                     </ul>
                     <div class="flex justify-center pt-8">
-                        <div class="text-4xl font-bold">$99</div>
-                        <div class="pl-1 pt-2 text-gray-300">/mo</div>
+                        <div class="text-4xl font-bold">{{ getPrice(plan.pricing[currentFrequency].price) }}</div>
+                        <div class="pl-1 pt-2 text-gray-300">{{ plan.pricing[currentFrequency].label }}</div>
                     </div>
                 </div>
+            </div>
 
-                <div class="text-white w-1/2">
-                    <h1 class="text-2xl font-bold">Starter</h1>
-                    <ul class="pt-4">
-                        <li>Benefit 1</li>
-                        <li>Benefit 2</li>
-                        <li>Benefit 3</li>
-                        <li>Benefit 4</li>
-                    </ul>
-                    <div class="flex justify-center pt-8">
-                        <div class="text-4xl font-bold">$199</div>
-                        <div class="pl-1 pt-2 text-gray-300">/mo</div>
-                    </div>
-                </div>
-
+            <div class="pt-4 text-center text-gray-400 text-sm text-bold">
+                <a href="#" @click.prevent="currency = 'usd' ">USD</a> |
+                <a href="#" @click.prevent="currency = 'eur' ">EUR</a> |
+                <a href="#" @click.prevent="currency = 'bdt' ">BDT</a>
             </div>
 
 
@@ -44,7 +38,79 @@
 
 <script>
     export default {
-        name: "Pricing"
+        name: "Pricing",
+
+        methods: {
+             getPrice(price) {
+              return this['currency' + this.currency.toUpperCase()](price);
+            },
+
+            currencyUSD(price) {
+                return '$' + price;
+            },
+
+            currencyEUR(price) {
+                return Math.ceil(price * 0.85) + '€';
+            },
+
+            currencyBDT(price) {
+                return '৳ ' + Math.ceil(price * 85) ;
+            },
+
+        },
+
+        data: function () {
+            return {
+                currentFrequency: 'monthly',
+                currency: 'usd',
+
+                plans: [
+                    {
+                        name: 'Starter',
+                        benefits: {
+                            monthly: ['Benefit 1', 'Benefit 2', 'Benefit 3'],
+                            yearly: ['Benefit 1', 'Benefit 2', 'Benefit 3', 'Benefit 4'],
+                            lifetime: ['Benefit 1', 'Benefit 2', 'Benefit 3', 'Benefit 4', 'Benefit 5'],
+                        },
+
+                        pricing: {
+                            monthly: { price: 99, label: '/mo'},
+                            yearly: { price: 499, label: '/yr'},
+                            lifetime: { price: 2299, label: ''}
+                        }
+                    },
+                    {
+                        name: 'Pro',
+                        benefits: {
+                            monthly: ['Benefit 1', 'Benefit 2', 'Benefit 3', 'Benefit 4'],
+                            yearly: ['Benefit 1', 'Benefit 2', 'Benefit 3', 'Benefit 4', 'Benefit 5'],
+                            lifetime: ['Benefit 1', 'Benefit 2', 'Benefit 3', 'Benefit 4', 'Benefit 5', 'Benefit 6'],
+                        },
+
+                        pricing: {
+                            monthly: { price: 199, label: '/mo'},
+                            yearly: { price: 999, label: '/yr'},
+                            lifetime: { price: 2999, label: ''}
+                        }
+                    },
+                    {
+                        name: 'Ultra',
+                        benefits: {
+                            monthly: ['Benefit 1', 'Benefit 2', 'Benefit 3', 'Benefit 4', 'Benefit 5'],
+                            yearly: ['Benefit 1', 'Benefit 2', 'Benefit 3', 'Benefit 4', 'Benefit 5', 'Benefit 6'],
+                            lifetime: ['Benefit 1', 'Benefit 2', 'Benefit 3', 'Benefit 4', 'Benefit 5', 'Benefit 6', 'Benefit 7'],
+                        },
+
+                        pricing: {
+                            monthly: { price: 249, label: '/mo'},
+                            yearly: { price: 1499, label: '/yr'},
+                            lifetime: { price: 3499, label: ''}
+                        }
+                    },
+                ]
+
+            }
+        }
     }
 </script>
 
